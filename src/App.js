a17e13c46435721502b1dcd26f46168a1a2bc796
Joshua Zimmerman <jpz6926@gmail.com>
2025-01-30 05:05:49 -0500

    added docker init configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import About from "./pages/About";
+import React from "react";
 import Projects from "./pages/Projects";
 import { Routes, Route } from "react-router-dom";
 
