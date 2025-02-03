cd4608549f27ebd2cee4409b7c3ec5f6f5a8bcc4
Joshua Zimmerman <jpz6926@gmail.com>
2025-02-02 23:52:41 -0500

    Some minor css adjustments; starting to refine the navbar animations

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,6 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="nav">
-      <Link to="/" className="site-nav">
-        BlackRose
-      </Link>
       <ul style={{ display: "inline-flex", padding: "0rem" }}>
         <li>
           <Link to="/about">About</Link>
