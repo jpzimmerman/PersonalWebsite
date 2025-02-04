8d6513d974f094fef3376567e431599bd660a95f
Joshua Zimmerman <jpz6926@gmail.com>
2025-02-03 23:58:08 -0500

    adjusting position of landing page main text

--- src/App.js
+++ src/App.js
@@ -19,21 +19,23 @@ function App() {
         <main>
           <section id="root"></section>
           <Navbar></Navbar>
-          <h3>Joshua Zimmerman, Software Engineer</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non
-            elementum arcu. Nullam porta nibh eu tempor convallis. Phasellus
-            eget faucibus enim. Sed efficitur urna nec risus dapibus, eget
-            commodo eros consectetur. Mauris blandit dui cursus purus dictum
-            fringilla sit amet ac metus. In quis nisi sollicitudin, venenatis
-            nunc in, rutrum arcu. Fusce ullamcorper lacus ut leo tincidunt
-            consequat. Nam eu lacus at lectus pretium congue. Nulla sagittis
-            euismod porta. Fusce eget nibh feugiat, interdum sem in, vestibulum
-            magna. Sed eget volutpat ligula, eget maximus sem. Aenean
-            sollicitudin sed odio nec tincidunt. Aliquam a dolor commodo,
-            pulvinar sapien ut, luctus arcu. Aenean porta purus mi, quis ornare
-            dolor elementum eu.{" "}
-          </p>
+          <section className="landing-body">
+            <h3>Joshua Zimmerman, Software Engineer</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non
+              elementum arcu. Nullam porta nibh eu tempor convallis. Phasellus
+              eget faucibus enim. Sed efficitur urna nec risus dapibus, eget
+              commodo eros consectetur. Mauris blandit dui cursus purus dictum
+              fringilla sit amet ac metus. In quis nisi sollicitudin, venenatis
+              nunc in, rutrum arcu. Fusce ullamcorper lacus ut leo tincidunt
+              consequat. Nam eu lacus at lectus pretium congue. Nulla sagittis
+              euismod porta. Fusce eget nibh feugiat, interdum sem in,
+              vestibulum magna. Sed eget volutpat ligula, eget maximus sem.
+              Aenean sollicitudin sed odio nec tincidunt. Aliquam a dolor
+              commodo, pulvinar sapien ut, luctus arcu. Aenean porta purus mi,
+              quis ornare dolor elementum eu.{" "}
+            </p>
+          </section>
         </main>
       </section>
       <Routes>
