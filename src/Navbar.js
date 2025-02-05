2be1d32b7af51d53c5098a8ffe775b5cbb490e56
Joshua Zimmerman <jpz6926@gmail.com>
2025-02-04 23:59:37 -0500

    Adding eslint-config-react-app package to resolve build issue

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,6 +4,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="nav">
+      <img src="https://cataas.com/cat" className="profile-image"></img>
       <ul style={{ display: "inline-flex", padding: "0rem" }}>
         <li>
           <Link to="/about">About</Link>
