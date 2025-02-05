f251255898a61341f01ba9552dd60d335735351f
Joshua Zimmerman <jpz6926@gmail.com>
2025-02-05 01:42:55 -0500

    added profile image, adjusted navbar css for positioning and responsiveness

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,17 +3,23 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <img src="https://cataas.com/cat" className="profile-image"></img>
-      <ul style={{ display: "inline-flex", padding: "0rem" }}>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>Blog</li>
-      </ul>
-    </nav>
+    <section className="navbar-container">
+      <img
+        src="https://cataas.com/cat"
+        alt="Candidate"
+        className="profile-image"
+      ></img>
+      <nav>
+        <ul style={{ display: "inline-flex", padding: "0rem" }}>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>Blog</li>
+        </ul>
+      </nav>
+    </section>
   );
 }
