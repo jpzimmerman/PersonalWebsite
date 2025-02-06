8f308b1ca5ed9117d1984ede8274e952b7823036
Joshua Zimmerman <jpz6926@gmail.com>
2025-02-06 04:53:17 -0500

    adjusting navbar size, moving landing content to 'home' component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import React from "react";
 import Projects from "./pages/Projects";
@@ -19,26 +20,11 @@ function App() {
         <main>
           <section id="root"></section>
           <Navbar></Navbar>
-          <section className="landing-body">
-            <h3>Joshua Zimmerman, Software Engineer</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non
-              elementum arcu. Nullam porta nibh eu tempor convallis. Phasellus
-              eget faucibus enim. Sed efficitur urna nec risus dapibus, eget
-              commodo eros consectetur. Mauris blandit dui cursus purus dictum
-              fringilla sit amet ac metus. In quis nisi sollicitudin, venenatis
-              nunc in, rutrum arcu. Fusce ullamcorper lacus ut leo tincidunt
-              consequat. Nam eu lacus at lectus pretium congue. Nulla sagittis
-              euismod porta. Fusce eget nibh feugiat, interdum sem in,
-              vestibulum magna. Sed eget volutpat ligula, eget maximus sem.
-              Aenean sollicitudin sed odio nec tincidunt. Aliquam a dolor
-              commodo, pulvinar sapien ut, luctus arcu. Aenean porta purus mi,
-              quis ornare dolor elementum eu.{" "}
-            </p>
-          </section>
+          <section className="landing-body"></section>
         </main>
       </section>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section className="landing-body">
+      <h3>Joshua Zimmerman, Software Engineer</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non
+        elementum arcu. Nullam porta nibh eu tempor convallis. Phasellus eget
+        faucibus enim. Sed efficitur urna nec risus dapibus, eget commodo eros
+        consectetur. Mauris blandit dui cursus purus dictum fringilla sit amet
+        ac metus. In quis nisi sollicitudin, venenatis nunc in, rutrum arcu.
+        Fusce ullamcorper lacus ut leo tincidunt consequat. Nam eu lacus at
+        lectus pretium congue. Nulla sagittis euismod porta. Fusce eget nibh
+        feugiat, interdum sem in, vestibulum magna. Sed eget volutpat ligula,
+        eget maximus sem. Aenean sollicitudin sed odio nec tincidunt. Aliquam a
+        dolor commodo, pulvinar sapien ut, luctus arcu. Aenean porta purus mi,
+        quis ornare dolor elementum eu.{" "}
+      </p>
+    </section>
+  );
+}
