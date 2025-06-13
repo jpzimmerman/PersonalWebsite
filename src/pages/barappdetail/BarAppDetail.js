65e524a6846ef7004128f80b8db0a4addd30ed09
Joshua Zimmerman <jpz6926@gmail.com>
2025-06-13 00:40:04 -0400

    CSS adjustment: moved navbar-specific CSS classes out of main css file and into navbar css file; adjusted css classes on detail page to to give the detail a slide-in, fade-in effect

--- src/pages/barappdetail/BarAppDetail.js
+++ src/pages/barappdetail/BarAppDetail.js
@@ -1,30 +1,33 @@
 import React from "react";
+import "./detail.css";
 
 export default function BarAppDetail() {
   return (
-    <section className="landing-body landing-body-detail">
+    <section className="landing-body landing-body-detail detail-page">
       <h3>BarNone - Cocktail Menu</h3>
       <hr></hr>
       <br></br>
-      BarNone is a full-stack Web application that allows users to order and
-      customize cocktails. The backend API serves a menu, accepts orders, and
-      allows an admin to maintain an inventory, while the decoupled frontend
-      allows a developer to build a cocktail menu around a given theme.
-      <p className="landing-body-text">
-        <h4>
-          <strong>Tech Stack</strong>
-        </h4>
-        <ul>
-          <li>Backend: .NET 9.0</li>
-          <li>Frontend: Angular 19</li>
-          <li>Inventory Micro-Frontend: React</li>
-          <li>Database: SQL Server, MySQL</li>
-          <li>Cloud platform: Azure</li>
-          <li>Infrastructure-as-Code: Terraform</li>
-          <li>Mobile app for vendor: React Native</li>
-        </ul>
-        <br></br>
-      </p>
+      <section className="detail-page-text-body">
+        BarNone is a full-stack Web application that allows users to order and
+        customize cocktails. The backend API serves a menu, accepts orders, and
+        allows an admin to maintain an inventory, while the decoupled frontend
+        allows a developer to build a cocktail menu around a given theme.
+        <p className="landing-body-text">
+          <h4>
+            <strong>Tech Stack</strong>
+          </h4>
+          <ul>
+            <li>Backend: .NET 9.0</li>
+            <li>Frontend: Angular 19</li>
+            <li>Inventory Micro-Frontend: React</li>
+            <li>Database: SQL Server, MySQL</li>
+            <li>Cloud platform: Azure</li>
+            <li>Infrastructure-as-Code: Terraform</li>
+            <li>Mobile app for vendor: React Native</li>
+          </ul>
+          <br></br>
+        </p>
+      </section>
     </section>
   );
 }
