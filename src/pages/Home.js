71003f2375bfcbab4adfe0483f7e8984b28300fb
Joshua Zimmerman <jpz6926@gmail.com>
2025-06-13 01:18:18 -0400

    Title card content update: added linkedin badge to socials section of title card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
 import githublogo from "../content/images/github-link-logo.svg";
+import linkedinlogo from "../content/images/LI-In-Bug.png";
 
 export default function Home() {
   return (
     <section className="landing-body">
       <h3>Joshua Zimmerman, Software Engineer</h3>
       <section className="socials-section">
+        <a
+          href="https://www.linkedin.com/in/joshua-zimmerman-235810247/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={linkedinlogo}
+            alt="LinkedIn profile"
+            className="socials-link-button"
+            target="_blank"
+            title="LinkedIn profile"
+          ></img>
+        </a>
         <a
           href="https://github.com/jpzimmerman"
           target="_blank"
