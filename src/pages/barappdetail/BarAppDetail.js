014545dd7355eedac9b12aa74067a9f7cbcb6a7f
Joshua Zimmerman <jpz6926@gmail.com>
2025-06-19 15:58:16 -0400

    CSS adjustment, feature prep: adjusts CSS to fix issue with navbar buttons being less clickable on mobile; sets up detail page so that we can make sections collapsible

--- src/pages/barappdetail/BarAppDetail.js
+++ src/pages/barappdetail/BarAppDetail.js
@@ -6,12 +6,17 @@ export default function BarAppDetail() {
     <section className="landing-body landing-body-detail detail-page">
       <h3>BarNone - Cocktail Menu</h3>
       <hr></hr>
-      <br></br>
       <section className="detail-page-text-body">
-        BarNone is a full-stack Web application that allows users to order and
-        customize cocktails. The backend API serves a menu, accepts orders, and
-        allows an admin to maintain an inventory, while the decoupled frontend
-        allows a developer to build a cocktail menu around a given theme.
+        <h4 className="collapsible-header">
+          <strong>Summary</strong>
+        </h4>
+        <span className="collapsible-content">
+          BarNone is a full-stack Web application that allows users to order and
+          customize cocktails. The backend API serves a menu, accepts orders,
+          and allows an admin to maintain an inventory, while the decoupled
+          frontend allows a developer to build a cocktail menu around a given
+          theme.
+        </span>
         <p className="landing-body-text">
           <h4>
             <strong>Tech Stack</strong>
