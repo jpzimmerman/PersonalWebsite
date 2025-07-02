abddb998821a8e02790ce16712b3f56f92ef9838
Joshua Zimmerman <jpz6926@gmail.com>
2025-07-02 02:51:10 -0400

    Content update: adding content to About Me page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -3,8 +3,37 @@ import React from "react";
 export default function About() {
   return (
     <section className="landing-body landing-body-detail">
-      <h3>About Me</h3>
+      <h2>About Me</h2>
       <hr></hr>
+      <h3>My History</h3>
+      I've always had a passion for creating software applications with a focus
+      on efficiency, user-friendliness, gamification where possible, and tight
+      designs that make the most of the hardware. Most of the experience I've
+      gained has been in the .NET ecosystem, but I've focused in recent years on
+      expanding my toolkit to specialize in MEAN, MERN, NoSQL, and Cloud
+      development. <br></br>
+      <br></br>After developing a few Excel VBA-based tools to improve my team's
+      workflow during my time as a Vendor Analyst at Rocket Mortgage, and
+      working with a team to develop a VR-based accessility training experience
+      as part of a rapid-development initiative, I was selected to participate
+      in a full-stack software development bootcamp at Grand Circus in Detroit.
+      There, I would combine everything I'd learned as a hobbyist up to that
+      point with industry-standard tools and workflows, and begin my career as a
+      software developer in earnest. <br></br>
+      <br></br>I worked for four years on the Document Services team at Rocket,
+      and created value wherever I could -- streamlining and refactoring the
+      company's document batch processor to achieve a 55% reduction in per-batch
+      processing time, becoming the team's subject matter expert on and
+      point-of-contact on complex third-party vendor software, automating away a
+      tedious manifest generation process for the team members I supported, and
+      contributing to the development and support of mission-critical Web
+      applications to make the day-to-day work of our business partners as easy
+      as possible.<br></br>
+      <br></br>Now that I've cut my teeth and gained this experience working on
+      an Agile team within an enterprise organization, I'm looking to bring my
+      skills and expertise to a new opportunity.
+      <br></br>
+      <br></br>
       <h3>My Philosophy</h3>I believe in assuming{" "}
       <strong> personal ownership</strong> over every application in my care,
       and in using my craft to deliver <strong>maximum impact</strong> for our
