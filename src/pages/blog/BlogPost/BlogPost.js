02bb5551141ff88cd9687403eccd9c686916948c
Joshua Zimmerman <jpz6926@gmail.com>
2025-07-06 01:32:56 -0400

    Blog post view update: renders blog post from a list within BlogView component. Next step is to load blog posts from app database.

--- src/pages/blog/BlogPost/BlogPost.js
+++ src/pages/blog/BlogPost/BlogPost.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./blogpost.css";
+import {
+  FaComment,
+  FaCommentDollar,
+  FaMessage,
+  FaThumbsDown,
+  FaThumbsUp,
+} from "react-icons/fa6";
 
 const BlogPost = ({ id, title, bodyText, numberOfReacts }) => {
   return (
-    <section>
+    <section className="landing-body">
       <section className="blog-card">
-        <p>Post Title</p>
-        <section>Body Text</section>
+        <h2>{title}</h2>
+        <section className="blog-post-body-text">{bodyText}</section>
+        <section className="engagement-section">
+          <FaThumbsUp className="reaction-button" />
+          <FaThumbsDown className="reaction-button" />
+          <FaComment className="comment-button" />
+        </section>
       </section>
     </section>
   );
--- src/pages/blog/BlogView.js
+++ src/pages/blog/BlogView.js
@@ -1,10 +1,27 @@
 import BlogPost from "./BlogPost/BlogPost";
 
 export default function BlogView() {
+  const blogPosts = [
+    {
+      id: 0,
+      title: "First Blog Post",
+      bodyText: "This is body text for the top blog post on the page",
+    },
+    {
+      id: 1,
+      title: "Second Blog Post",
+      bodyText:
+        "This is body text for the bottom blog post on the page. It is meant to span multiple lines and test spacing. Next step is to load blog posts from the database using our Web service.",
+    },
+  ];
+
   return (
     <section>
-      <p>Blog works!</p>
-      <BlogPost />
+      {blogPosts.map((item, index) => (
+        <section key={index}>
+          <BlogPost title={item.title} bodyText={item.bodyText} />
+        </section>
+      ))}
     </section>
   );
 }
