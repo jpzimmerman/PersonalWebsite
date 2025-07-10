7c6b94ef6664b4d59092282e12fe9bfdc68c5956
Joshua Zimmerman <jpz6926@gmail.com>
2025-07-09 23:15:02 -0400

    Content update: adjusted About Me page content, corrects a few typos and expands the copy

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -12,25 +12,29 @@ export default function About() {
       gained has been in the .NET ecosystem, but I've focused in recent years on
       expanding my toolkit to specialize in MEAN, MERN, NoSQL, and Cloud
       development. <br></br>
-      <br></br>After developing a few Excel VBA-based tools to improve my team's
-      workflow during my time as a Vendor Analyst at Rocket Mortgage, and
-      working with a team to develop a VR-based accessility training experience
-      as part of a rapid-development initiative, I was selected to participate
-      in a full-stack software development bootcamp at Grand Circus in Detroit.
+      <br></br>I worked in a data entry role at Rocket Mortgage for several
+      years, but I still brought a passion for technology and an eye for process
+      improvement to the work in any way I could. After developing a few Excel
+      VBA-based tools to improve my team's workflow, and working with a team to
+      develop a VR accessibility training experience as part of a
+      rapid-development initiative, I was selected to participate in a
+      full-stack software development bootcamp at Grand Circus in Detroit.
       There, I would combine everything I'd learned as a hobbyist up to that
-      point with industry-standard tools and workflows, and begin my career as a
-      software developer in earnest. <br></br>
+      point with industry-standard tools and workflows, and I began my career as
+      a software engineer in earnest. <br></br>
       <br></br>I worked for four years on the Document Services team at Rocket,
-      and created value wherever I could -- streamlining and refactoring the
-      company's document batch processor to achieve a 55% reduction in per-batch
-      processing time, becoming the team's subject matter expert on and
-      point-of-contact on complex third-party vendor software, automating away a
-      tedious manifest generation process for the team members I supported, and
-      contributing to the development and support of mission-critical Web
-      applications to make the day-to-day work of our business partners as easy
-      as possible.<br></br>
-      <br></br>Now that I've cut my teeth and gained this experience working on
-      an Agile team within an enterprise organization, I'm looking to bring my
+      and set about the business of creating value wherever I could. I
+      streamlined and refactored the company's document batch processor to
+      achieve a 55% reduction in per-batch processing time, I became the team's
+      subject matter expert on and point-of-contact on complex third-party
+      vendor software, and shared the knowledge so that we could achieve
+      long-wishlisted initiatives within the company. I wrote and maintained a
+      "little app that could" that automated away a tedious manifest generation
+      process for the team members I supported, and I contributed to the
+      development and support of mission-critical Web applications to make the
+      day-to-day work of our business partners as easy as possible.<br></br>
+      <br></br>Now that I've cut my teeth and gained this experience in Agile
+      development within an enterprise organization, I'm looking to bring my
       skills and expertise to a new opportunity.
       <br></br>
       <br></br>
